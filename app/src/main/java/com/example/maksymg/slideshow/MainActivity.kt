07430598b62a72ce5cb60ccfafbb5cfa1095324f
maksymg <max.malina13@gmail.com>
2018-06-04 17:38:54 +0300

    add request permission

--- app/src/main/java/com/example/maksymg/slideshow/MainActivity.kt
+++ app/src/main/java/com/example/maksymg/slideshow/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.maksymg.slideshow
 
+import android.Manifest
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.annotation.NonNull
 import android.support.v4.app.ActivityCompat
 import android.view.View
 import android.view.animation.AnimationUtils
@@ -26,31 +27,37 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
         initAnalogClock()
         initViewFlipper()
 
-        photoViewModel = ViewModelProviders.of(this).get(PhotoViewModel::class.java)
+        requestExternalStoragePermission()
+    }
 
-        photoViewModel!!.getAllPhotos().observe(this, Observer { photos: List<Media>? ->
-            for(photo in photos!!) {
-                var mediaView = View(this)
-                when(photo.mediaType) {
-                    MediaType.IMAGE -> {
-                        mediaView = ImageView(this)
-                        mediaView.scaleType = ImageView.ScaleType.FIT_CENTER
-                        Picasso.get().load("file:///" + photo.path).into(mediaView)
-                    }
-
-                    MediaType.VIDEO -> {
-                        mediaView = VideoView(this)
-                        mediaView.setVideoPath(photo.path)
-                        mediaView.start()
-                    }
-                }
-                viewFlipper.addView(mediaView)
-            }
-        })
-
+    private fun requestExternalStoragePermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 0)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if(grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
+            photoViewModel = ViewModelProviders.of(this).get(PhotoViewModel::class.java)
+
+            photoViewModel!!.getAllPhotos().observe(this, Observer { photos: List<Media>? ->
+                for (photo in photos!!) {
+                    var mediaView = View(this)
+                    when (photo.mediaType) {
+                        MediaType.IMAGE -> {
+                            mediaView = ImageView(this)
+                            mediaView.scaleType = ImageView.ScaleType.FIT_CENTER
+                            Picasso.get().load("file:///" + photo.path).into(mediaView)
+                        }
+
+                        MediaType.VIDEO -> {
+                            mediaView = VideoView(this)
+                            mediaView.setVideoPath(photo.path)
+                            mediaView.start()
+                        }
+                    }
+                    viewFlipper.addView(mediaView)
+                }
+            })
+        }
     }
 
     private fun initAnalogClock() {
